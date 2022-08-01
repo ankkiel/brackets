763f46c42f3c89f7ab16d176f004f23e2c80f5d1
Anastasia Ermolova <anastasia_ermolova@bk.ru>
2022-08-01 13:49:42 +0300

    feat: implement brackets check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openedBracket, closedBracket;
+  let lengthStr = str.length;
+  
+  for ([openedBracket, closedBracket] of bracketsConfig) {
+    str = str.split(`${openedBracket}${closedBracket}`).join('');
+  }
+  
+  return str.length == 0 ? true: str.length == lengthStr ? false: check(str, bracketsConfig);
 }
